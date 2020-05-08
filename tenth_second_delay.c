4f5db967739e98a880c5a62a0c09fab2ffc088f6
Trenton Morgan <tpm7qq@mst.edu>
2020-05-08 17:12:12 -0500

    added delay function

---
+++ tenth_second_delay.c
@@ -0,0 +1,27 @@
+//Using timer/counter 0, create a 0.1 s delay to be used as the tick rate in 
+//the stopwatch and timer features.
+
+void Tenth_Second_Delay()
+{
+	
+
+	
+}
+
+
+Tenth_Second_Delay()
+{
+	configure timer settings;
+
+	//for loop needed only if # of timer ticks > 256 with prescale of 1024
+	//if loop is needed, enable CTC mode to reset counter on compare match
+	for x iterations
+	{
+		while(timer_compare_match = false)
+		{
+			//do nothing
+		}
+	}
+
+	return 0;
+}
