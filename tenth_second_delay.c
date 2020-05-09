70083a47440e84550ed83d4f9ae519f6707516f2
Trenton Morgan <tpm7qq@mst.edu>
2020-05-09 17:43:29 -0500

    use overflow for delay

--- tenth_second_delay.c
+++ tenth_second_delay.c
@@ -4,22 +4,21 @@ void Tenth_Second_Delay()
 {
 	//Timer needs to run 50 times to achieve desired delay
 	unsigned char LOOP_COUNT = 50;
-	
-	//Initialize output compare register to 124 (0->124 = 125 ticks)
-	OCR0A = 124;
-	//Enable output compare interrupt
-	TIMSK0 = (1<<OCIE0A);
-	//Turn on timer configured for CTC mode and prescale of 256
-	TCCR0B = (1<<WGM00) | (1<<CS02);
+
+	//Turn on timer configured for normal mode and prescale of 256
+	TCCR0B = (1<<CS02);
 
 	//Run timer for 0.1s (50 * 125 * 256 = 1600000 => 0.1s)
 	for(unsigned char i = 0; i < LOOP_COUNT; i++)
 	{
-		//Wait until compare match flag is raised
-		while((TIFR0 & (1<<OCF0A)) != 0);
+		//Initialize/reset counter value
+		TCNT0 = -125;
 
-		//Clear output compare flag
-		TIFR0 = (1<<OCF0A);
+		//Wait until compare match flag is raised
+		while(!(TIFR0 & (1<<TOV0)));
+
+		//Clear overflow flag
+		TIFR0 = (1<<TOV0);
 	}
 
 	//Turn off timer
